bad7a7e371c6bb2f16f073f9731df7d195cbe15d
jeppeoberg <jeob1000@stud.ek.dk>
2026-01-30 00:19:41 +0100

    final commit

---
+++ src/Uge1Variables.java
@@ -0,0 +1,35 @@
+//Working with variables ugeopgave 1
+// https://github.com/Dat1Cphbusiness/ugeopgave-1-variables-and-datatypes
+
+
+//Opgave 1:
+double item1;
+double item2;
+double item3;
+double itemSum;
+double tax;
+double taxSum;
+
+//Opgave 2
+
+
+
+
+void main() {
+    item1 = 15.00;
+    item2 = 25.50;
+    item3 = 30.00;
+    tax = 1.25;
+    itemSum = (item1 + item2 + item3);
+    taxSum = itemSum*tax;
+    System.out.println(itemSum);
+    System.out.println(taxSum);
+
+
+
+
+
+
+
+
+}
---
+++ src/uge1OperatorsExpressions.java
@@ -0,0 +1,90 @@
+// Opgave 4
+int guest = 558;
+int totalCupcake = 17;
+int cupcakeDivided = totalCupcake / guest;
+int remainingCakes = totalCupcake % guest;
+String line = "                                                  ";
+
+//Opgave 5: Build greeting message
+
+String firstName = "Anna";
+int age = 25;
+
+
+// Opgave 6
+int score = 100;
+
+// Opgave 8
+int price = 15;
+
+
+// Opgave 8
+String firstName1 = "Anna";
+String lastName = "Jensen";
+
+// Opgave 11
+
+String product = "Laptop";
+double price11 = 7999.99;
+//nt stock = ;
+boolean inStock = true;
+
+
+//----------
+
+void main() {
+
+//------------------------------------------------
+
+    System.out.println("Opgave 1:");
+
+    System.out.println("each guest gets" + " " + cupcakeDivided + " cupcakes");
+    System.out.println(remainingCakes + " " + "cupcakes left over");
+    System.out.println();
+
+//------------------------------------------------
+
+    System.out.println("Opgave 3:");
+
+    System.out.println(score);
+    score += 50;
+    System.out.println(score);
+    score = score + 25;
+    System.out.println(score);
+    System.out.println(line);
+
+//------------------------------------------------
+
+   // System.out.println("Opgave 4");
+
+
+//"Hello, ", navnet, "! ", "You are ", alderen og " years old."
+
+
+//------------------------------------------------
+
+    System.out.println("Opgave 8");
+    price *= 2;
+    System.out.println(price);
+
+
+    System.out.println(line);
+
+//------------------------------------------------
+    System.out.println("Opgave 11");
+    System.out.println(line);
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
